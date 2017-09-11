9a2ab872f2891ce89f5cef1bdbee02a030e23970
kp201760 <rajinderkaur.dec6@gmail.com>
2017-09-11 14:33:57 +1000

    updation in notifyCarParkEvent() method in EntryController module
    insert display method for proper functioning

--- bcccp/carpark/entry/EntryController.java
+++ bcccp/carpark/entry/EntryController.java
@@ -322,6 +322,7 @@ public class EntryController
 	public void notifyCarparkEvent() {
 		if (state_ == STATE.FULL) {
 			if (!carpark.isFull()) {
+				System.out.println("push button");
 				setState(STATE.WAITING);
 			}
 		}
