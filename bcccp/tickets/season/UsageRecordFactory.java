3d9bb67fafee53b7c0b2b653b569f75829036c8b
kp201760 <30428224+kp201760@users.noreply.github.com>
2017-09-13 18:26:42 +1000

    Implementation in constructor
    Throw exception for invalid ticketId

--- bcccp/tickets/season/UsageRecordFactory.java
+++ bcccp/tickets/season/UsageRecordFactory.java
@@ -4,6 +4,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
+		if(ticketId==null && ticketId<=0) throw new Exception("Invalid Ticket ID");
 		return new UsageRecord(ticketId, startDateTime);
 	}
 
