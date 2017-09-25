0ea96166a892ca546f37979e513405139d23c969
kp201760 <30428224+kp201760@users.noreply.github.com>
2017-09-25 15:40:24 +1000

    integration testing AdhocTicketDAO

---
+++ integration testing AdhocTicketDAO/IAdhocTicketDAO.java
@@ -0,0 +1,12 @@
+
+
+import java.util.List;
+
+public interface IAdhocTicketDAO {
+	
+	public IAdhocTicket createTicket(String carparkId);
+	public IAdhocTicket findTicketByBarcode(String barcode);
+	public List<IAdhocTicket> getCurrentTickets();
+
+
+}
