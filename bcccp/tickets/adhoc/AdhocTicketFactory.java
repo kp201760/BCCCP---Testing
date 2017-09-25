865b74fe109bd17230c5e3fc4065ed5bc3f98015
kp201760 <rajinderkaur.dec6@gmail.com>
2017-09-25 14:44:50 +1000

    implementation of make method in adhocTicketFactory

--- bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -3,7 +3,11 @@ package bcccp.tickets.adhoc;
 public class AdhocTicketFactory implements IAdhocTicketFactory {
 
 	@Override
-	public IAdhocTicket make(String carparkId, int ticketNo) {
+	public IAdhocTicket make(String carparkId, int ticketNo) throws Exception {
+		if(carparkId == null || carparkId == "")
+			throw Exception("invalid carparkId");
+		
+			
 		String barcode = "A" + Integer.toHexString(ticketNo);
 		return new AdhocTicket(carparkId, ticketNo, barcode);
 	}
